f8b113d99a3da76a8c6a4b5a683cdea2d344b8f8
Yoggy-Kumar <146549860+Yoggy-Kumar@users.noreply.github.com>
2023-10-01 02:19:44 +0000

    changes done

---
+++ basics/variables.js
@@ -0,0 +1,13 @@
+const accountId = "4545"
+let accountEmail ="yoggy241201@gmail.com"
+var accountPassword = "4545"
+accontCity ="bareillly"
+ //accountId = 2//not allowed
+
+ accountEmail = "hc@hc.com"
+ accountPassword = "7887"
+ accountCity = "rampur"
+
+ console.log(accountId);
+
+ console.table([accountId,accountEmail,accountPassword,accountCity]);
